23d30a4d565da4fa54e63c8e4530608ffa2b440e
Ninespring <xiaolongshen.stuff@gmail.com>
2015-05-05 15:50:57 +0800

    make the inherit work, modify temp file for easier calling.
    Add fann_wifi_train for easier training and load_wifi_data to load in certain type of data.

--- NN_Classifier/NN_WiFi_Test_Class.py
+++ NN_Classifier/NN_WiFi_Test_Class.py
@@ -1,10 +1,51 @@
 __author__ = 'Xiaolong Shen sxl@nexdtech.com'
 from NN_Test_Class import *
-
+from loadfile import LoadWifiData
 
 class NeuralNetworkWifiTest(NeuralNetworkTest):
 	def __init__(self):
+		NeuralNetworkTest.__init__(self)
+		self.load_in = LoadWifiData()
 		pass
+
+	def load_wifi_data_old(self, dir_path, save_path="wifi_floor.npz"):
+		file_list = os.listdir(dir_path)
+		floor_data = {}
+		for file_name in file_list:
+			if file_name.endswith("wp"):
+				name, ext = os.path.splitext(file_name)
+				print name
+				wifi_file = name + ".wifi"
+				wifi_file = os.path.join(dir_path, wifi_file)
+				wp_file = os.path.join(dir_path, file_name)
+				floor_data[name] = self.load_in.extract(wp_file, wifi_file)
+				# temp = load_in.extract_raw(wp_file, wifi_file)
+				# print temp
+
+		# Get All WiFi Listed
+		t_bind = []
+		for x in floor_data:
+			t_bind += floor_data[x].wifi_list
+		wifi_bld_list = list(set(t_bind))
+
+		TD = 0
+
+		for x in floor_data:
+			D = floor_data[x].wifi_matrix.shape[0]
+			TD += D
+			L = len(wifi_bld_list)
+			t_mat = zeros((D, L))
+			for i, j in enumerate(floor_data[x].wifi_list):
+				ind = wifi_bld_list.index(j)
+				t_mat[:, ind] = floor_data[x].wifi_matrix[:, i]
+			floor_data[x].wifi_bld_mat = t_mat
+
+		for xx in floor_data:
+			print floor_data[xx].wifi_bld_mat
+
+		savez(save_path, floor=floor_data, wifilist=wifi_bld_list, entry_num=TD)
+
+		return floor_data, wifi_bld_list, TD
 
 	def fann_wifi_test_recovered(self, class_num, test_data, normalize=True, savepath="./temp_save.conf"):
 		if not os.path.exists(savepath):
@@ -39,3 +80,21 @@ class NeuralNetworkWifiTest(NeuralNetworkTest):
 			res[i, :] = net.run(test_array.round())
 		self.net = net
 		return res
+
+	def fann_wifi_train(self, floor_data, wifi_bld_list, td):
+		all_data_mat = zeros((td, len(wifi_bld_list)))
+		all_data_cls = zeros((td, len(floor_data.keys())))
+
+		pos_pt = 0
+		cls_dict = {}
+
+		for i, xx in enumerate(floor_data):
+			d = floor_data[xx].wifi_bld_mat.shape[0]
+			all_data_mat[pos_pt:pos_pt + d, :] = floor_data[xx].wifi_bld_mat
+			all_data_cls[pos_pt:pos_pt + d, i] = 1
+			pos_pt += d
+			cls_dict[i] = xx
+		net = self.fann_learn_save_norm(all_data_mat, all_data_cls, "wifi_floor.conf", False)
+		self.net = net
+		return net, cls_dict
+
--- NN_Classifier/temp.py
+++ NN_Classifier/temp.py
@@ -8,64 +8,15 @@ load_in = LoadWifiData()
 
 
 #
-# ori_path = "/Users/admin/Code/ProjectCode/Algorithm/Classification_Attemption/Data/wifidata"
-# file_list = os.listdir(ori_path)
-# floor_data = {}
-# for file_name in file_list:
-# 	if file_name.endswith("wp"):
-# 		name, ext = os.path.splitext(file_name)
-# 		print name
-# 		wifi_file = name + ".wifi"
-# 		wifi_file = os.path.join(ori_path, wifi_file)
-# 		wp_file = os.path.join(ori_path, file_name)
-# 		floor_data[name] = load_in.extract(wp_file, wifi_file)
-# 		# temp = load_in.extract_raw(wp_file, wifi_file)
-# 		# print temp
-#
-# # Get All WiFi Listed
-# t_bind = []
-# for x in floor_data:
-# 	t_bind += floor_data[x].wifi_list
-# wifi_bld_list = list(set(t_bind))
-#
-# TD = 0
-#
-# for x in floor_data:
-# 	D = floor_data[x].wifi_matrix.shape[0]
-# 	TD += D
-# 	L = len(wifi_bld_list)
-# 	t_mat = zeros((D, L))
-# 	for i, j in enumerate(floor_data[x].wifi_list):
-# 		ind = wifi_bld_list.index(j)
-# 		t_mat[:, ind] = floor_data[x].wifi_matrix[:, i]
-# 	floor_data[x].wifi_bld_mat = t_mat
-#
-# for x in floor_data:
-# 	print floor_data[x].wifi_bld_mat
-#
-# savez("wifi_floor.npz", floor=floor_data, wifilist=wifi_bld_list, entry_num=TD)
+ori_path = "/Users/admin/Code/ProjectCode/Algorithm/Classification_Attemption/Data/wifidata"
+save_path = "wifi_floor.npz"
+floor_data, wifi_bld_list, TD = util.load_wifi_data_old(ori_path)
 
-
-data = load('wifi_floor.npz')
+data = load(save_path)
 floor_data = data['floor'].item()
 wifi_bld_list = list(data['wifilist'])
 TD = data['entry_num']
-# # print floor_data['F1']
-# print len(floor_data.keys())
-# print len(wifi_bld_list)
-# #
-all_data_mat = zeros((TD, len(wifi_bld_list)))
-all_data_cls = zeros((TD, len(floor_data.keys())))
 
-pos_pt = 0
-cls_dict = {}
-
-for i, x in enumerate(floor_data):
-	D = floor_data[x].wifi_bld_mat.shape[0]
-	all_data_mat[pos_pt:pos_pt+D, :] = floor_data[x].wifi_bld_mat
-	all_data_cls[pos_pt:pos_pt+D, i] = 1
-	pos_pt += D
-	cls_dict[i] = x
 # #
 # # print all_data_cls
 # #
@@ -74,6 +25,7 @@ for i, x in enumerate(floor_data):
 # print cls_dict
 #
 
+net, cls_dict = util.fann_wifi_train(floor_data, wifi_bld_list, TD)
 
 
 
